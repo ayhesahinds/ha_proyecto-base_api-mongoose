1c7a87ce4d920d9a18d3f0947fe75d076c3a59ee
ayhesahinds <ayhesahinds@gmail.com>
2025-02-21 15:04:46 -0300

    seeders

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -16,18 +16,58 @@
 
 const faker = require("@faker-js/faker").fakerES;
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+const crypto = require("crypto");
 
 module.exports = async () => {
   const users = [];
+  const userPassword = await bcrypt.hash("1234", 10);
+  const imgDir = path.join(__dirname, "/../public/img");
 
   for (let i = 0; i < 100; i++) {
-    users.push({
-      firstname: faker.person.firstName(),
-      lastname: faker.person.lastName(),
+    const firstName = faker.person.firstName();
+    const lastName = faker.persona.lastName();
 
-      
-   
-    });
+    // Generar un nombre único para la imagen utilizando crypto
+    const imageFileName = `${crypto.randomUUID()}.jpg`;
+
+    // Usar faker.image.urlPicsumPhotos para obtener una URL de imagen aleatoria
+    const imageUrl = faker.image.urlPicsumPhotos({ width: 150, height: 150 });
+
+
+ // Descargar la imagen y guardarla en la carpeta public/img
+ try {
+  const response = await axios({
+    url: imageUrl,
+    method: "GET",
+    responseType: "stream",
+  });
+
+  const writer = fs.createWriteStream(path.join(imgDir, imageFileName));
+  response.data.pipe(writer);
+
+  // Esperamos a que se termine de descargar la imagen
+  await new Promise((resolve, reject) => {
+    writer.on("finish", resolve);
+    writer.on("error", reject);
+  });
+
+
+    users.push({
+      firstname: firstName,
+      lastname: lastName,
+      username: faker.internet.username(),
+      password: userPassword,
+      email: faker.internet.email({ firstName, lastName, provider: "gmail.com" }),
+      Bio: faker.lorem.sentences(3),
+      image: imageFileName, // Guardamos el nombre del archivo de la imagen
+      });
+    } catch (error) {
+      console.error(`Error descargando la imagen: ${error.message}`);
+    }
   }
 
   await User.insertMany(users);
