195ae2f987c3e02c04a8cd0d0d14269cc9752d86
ayhesahinds <ayhesahinds@gmail.com>
2025-02-24 17:38:04 -0300

    español a ingles en respuestas

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,10 @@ const jwt = require("jsonwebtoken");
 
 async function getToken(req, res) {
   const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.json({ msg: "Credenciales incorrectas" });
+  if (!user) return res.json({ msg: "Invalid Credentials" });
 
   const isValidPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!isValidPassword) return res.json({ msg: "Credenciales incorrectas" });
+  if (!isValidPassword) return res.json({ msg: "Invalid Credentials" });
 
   const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET);
   return res.json(token);
--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -3,7 +3,7 @@ const { expressjwt: checkJwt } = require("express-jwt");
 function checkAuth(req, res, next) {
   checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] })(req, res, (error) => {
     if (!req.auth) {
-      return res.json({ msg: "Inicia sesión." });
+      return res.json({ msg: "Please, login" });
     }
     return next();
   });
