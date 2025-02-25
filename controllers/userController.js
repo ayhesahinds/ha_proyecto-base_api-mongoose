73648eb332389fa154cc865d6eb5c7ef27603be0
ayhesahinds <ayhesahinds@gmail.com>
2025-02-25 17:21:01 -0300

    update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,21 +61,56 @@ async function store(req, res) {
 
 // Update the specified resource in storage.
 async function update(req, res) {
-  try {
-    const id = req.params.id;
-    const { firstname, lastname, age, username, email, bio, password, avatar } = req.body;
-    const updateFields = { firstname, lastname, age, username, email, bio, avatar };
+  const { id } = req.params;
 
-    if (password) {
-      updateFields.password = await bcrypt.hash(password, 10);
+  const form = formidable({
+    multiples: true,
+    uploadDir: __dirname + "/../public/img",
+    keepExtensions: true,
+  });
+
+  form.parse(req, async (err, fields, files) => {
+    try {
+      if (err) return err;
+
+      const { firstname, lastname, username, password, bio, age, email } = fields;
+      const { avatar } = files;
+      const updateFields = {
+        firstname,
+        lastname,
+        username,
+        password,
+        age,
+        bio,
+        email,
+        avatar,
+      };
+      if (password) {
+        updateFields.password = await bcrypt.hash(password, 10);
+      }
+
+     
+
+      const user = await User.findByIdAndUpdate(id, updateFields);
+
+      if (user.avatar) {
+        const imagePath = __dirname + "/../public/img/" + user.avatar;
+        fs.unlink(imagePath, (err) => {
+          if (err) {
+            console.error("Error deleting file:", err);
+          }
+        });
+      }
+
+      return res.json({ msg: "User updated succesfully" });
+    } catch (error) {
+      res.status(500).json({ msg: "User updated unsuccesfully" });
     }
-
-    const updatedUser = await User.findByIdAndUpdate(id, updateFields, { new: true });
-    return res.json({ updatedUser });
-  } catch (error) {
-    return res.status(500).json({ msg: error.message });
-  }
+  });
 }
+
+
+
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {
