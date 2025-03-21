a85ac483d381b1c4874ac0ec782423c1cc54e068
ayhesahinds <ayhesahinds@gmail.com>
2025-03-21 15:27:17 -0300

    userseeders

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -15,27 +15,76 @@
  */
 
 const faker = require("@faker-js/faker").fakerES;
-const bcrypt = require("bcryptjs");
-
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+
 
 module.exports = async () => {
   const users = [];
   const hashedPassword = await bcrypt.hash("1234", 10);
+  const imgDir = path.join(__dirname, "/../public/img");
+  
 
-  for (let i = 0; i < 100; i++) {
-    const firstname = faker.person.firstName();
-    const lastname = faker.person.lastName();
 
-    users.push({
-      firstname,
-      lastname,
-      username: `${firstname.toLowerCase()}${lastname.toLowerCase()}`,
-      password: hashedPassword,
-      email: faker.internet.email(),
-      bio: faker.lorem.sentence(),
-      avatar: faker.image.avatar(),
-    });
+
+  for (let i = 0; i <= 100; i++) {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const age = faker.number.int({ min: 18, max: 80 });
+    const email = faker.internet.email({ firstName, lastName, provider: "gmail.com" });
+    const imageHead = faker.image.urlLoremFlickr({ width: 908, height: 250, category: 'nature' });
+    const numberUsername = faker.number.int({ max: 300 });
+    const wordUsername = faker.word.verb({ length: { min: 5, max: 12 } });
+    const username = faker.helpers.fake({ wordUsername, numberUsername });
+  
+    
+
+    
+  
+    // Generar un nombre único para la imagen utilizando crypto
+    const imageFileName = `${crypto.randomUUID()}.jpg`;
+
+    // Usar faker.image.personPortrait para obtener una URL de imagen aleatoria y se define el tamaño
+    const imageUrl = faker.image.personPortrait({ size: '256' });
+
+    // Descargar la imagen y guardarla en la carpeta public/img
+    try {
+      const response = await axios({
+        url: imageUrl,
+        method: "GET",
+        responseType: "stream",
+      });
+
+      const writer = fs.createWriteStream(path.join(imgDir, imageFileName));
+      response.data.pipe(writer);
+
+      // Esperamos a que se termine de descargar la imagen
+      await new Promise((resolve, reject) => {
+        writer.on("finish", resolve);
+        writer.on("error", reject);
+      });
+
+
+ 
+      users.push({
+        firstname: firstName,
+        lastname: lastName,
+        age: age,
+        email: email,
+        username: username,
+        bio: faker.lorem.sentence(10),
+        password: hashedPassword,
+        avatar: imageFileName,
+        backgroundUser: imageHead,
+      
+
+      });
+    } catch (error) {
+      console.error(`Error descargando la imagen: ${error.message}`);
+    }
   }
 
   await User.insertMany(users);
